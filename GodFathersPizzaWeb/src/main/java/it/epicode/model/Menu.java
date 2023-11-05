08aae954c9b8bce722ef5fcfaa0a28ea79d7f685
Christian Niro <91950577+chry2512@users.noreply.github.com>
2023-11-05 13:59:46 +0100

    Update Menu.java

--- GodFathersPizzaWeb/src/main/java/it/epicode/model/Menu.java
+++ GodFathersPizzaWeb/src/main/java/it/epicode/model/Menu.java
@@ -13,33 +13,33 @@ public class Menu {
 	List<Franchise> menuFranchise = new ArrayList<>();
 	
 	
-//	public List<Pizza> getMenuPizza() {
-//		return menuPizza;
-//	}
-//	
-//	public List<PizzaTopping> getMenuTopping() {
-//		return menuTopping;
-//	}
-//	
-//	public List<Drink> getMenuDrink() {
-//		return menuDrink;
-//	}
-//	
-//	public List<Franchise> getMenuFranchise() {
-//		return menuFranchise;
-//	}
+	public List<Pizza> getMenuPizza() {
+		return menuPizza;
+	}
 	
-//	public void creaMenu() {
-//		menuPizza.add(new PizzaHawaiian());
-//		menuPizza.add(new PizzaMargherita());
-//		menuPizza.add(new PizzaSalami());
-//		menuPizza.add(new PizzaFamilySize(new PizzaMargherita()));
-//		menuTopping.add(new ToppingCheese(new PizzaMargherita()));
-//		menuTopping.add(new ToppingHam(new PizzaSalami()));
-//		menuDrink.add(new DrinkLemonade());
-//		menuFranchise.add(new FranchiseMug());
-//
-//	}
+	public List<PizzaTopping> getMenuTopping() {
+		return menuTopping;
+	}
+	
+	public List<Drink> getMenuDrink() {
+		return menuDrink;
+	}
+	
+	public List<Franchise> getMenuFranchise() {
+		return menuFranchise;
+	}
+	
+	public void creaMenu() {
+		menuPizza.add(new PizzaHawaiian());
+		menuPizza.add(new PizzaMargherita());
+		menuPizza.add(new PizzaSalami());
+		menuPizza.add(new PizzaFamilySize(new PizzaMargherita()));
+		menuTopping.add(new ToppingCheese(new PizzaMargherita()));
+		menuTopping.add(new ToppingHam(new PizzaSalami()));
+		menuDrink.add(new DrinkLemonade());
+		menuFranchise.add(new FranchiseMug());
+
+	}
 	
 	
 	
