879008d6bf67e61110b024a6041a2abae68f7933
Christian Niro <91950577+chry2512@users.noreply.github.com>
2023-11-05 14:03:49 +0100

    Update Tavolo.java

--- GodFathersPizzaWeb/src/main/java/it/epicode/model/Tavolo.java
+++ GodFathersPizzaWeb/src/main/java/it/epicode/model/Tavolo.java
@@ -12,38 +12,36 @@ public class Tavolo {
 	private Integer numeroMassimoCoperti;
 	private boolean occupato;
 	
-//	public Tavolo(Integer numero, Integer numeroMassimoCoperti, boolean occupato) {
-//		super();
-//		this.numero = numero;
-//		this.numeroMassimoCoperti = numeroMassimoCoperti;
-//		this.occupato = occupato;
-//	}
-//
-//	public Integer getNumero() {
-//		return numero;
-//	}
-//
-//	public void setNumero(Integer numero) {
-//		this.numero = numero;
-//	}
-//
-//	public Integer getNumeroMassimoCoperti() {
-//		return numeroMassimoCoperti;
-//	}
-//
-//	public void setNumeroMassimoCoperti(Integer numeroMassimoCoperti) {
-//		this.numeroMassimoCoperti = numeroMassimoCoperti;
-//	}
-//
-//	public boolean isOccupato() {
-//		return occupato;
-//	}
-//
-//	public void setOccupato(boolean occupato) {
-//		this.occupato = occupato;
-//	}
-	
-	
-	
+	public Tavolo(Integer numero, Integer numeroMassimoCoperti, boolean occupato) {
+		super();
+		this.numero = numero;
+		this.numeroMassimoCoperti = numeroMassimoCoperti;
+		this.occupato = occupato;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getNumeroMassimoCoperti() {
+		return numeroMassimoCoperti;
+	}
+
+	public void setNumeroMassimoCoperti(Integer numeroMassimoCoperti) {
+		this.numeroMassimoCoperti = numeroMassimoCoperti;
+	}
+
+	public boolean isOccupato() {
+		return occupato;
+	}
+
+	public void setOccupato(boolean occupato) {
+		this.occupato = occupato;
+	}
+		
 	
 }
