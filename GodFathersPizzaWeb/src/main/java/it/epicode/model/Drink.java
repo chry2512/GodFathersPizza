b17cbd21aff8a252b066fd057642f5d3423d084e
Christian Niro <91950577+chry2512@users.noreply.github.com>
2023-11-05 13:57:05 +0100

    Update Drink.java

--- GodFathersPizzaWeb/src/main/java/it/epicode/model/Drink.java
+++ GodFathersPizzaWeb/src/main/java/it/epicode/model/Drink.java
@@ -9,54 +9,54 @@ import lombok.Setter;
 @AllArgsConstructor
 
 public class Drink implements FoodItem {
-	private String name;
-	private Double price;
-	private Double calories;
-	
-	
-//	public Drink(String name, Double price, Double calories) {
-//		this.name = name;
-//		this.price = price;
-//		this.calories = calories;
-//	}
+    private String name;
+    private Double price;
+    private Double calories;
 
 
-//	public String getName() {
-//		return name;
-//	}
-//
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//
-//	public Double getPrice() {
-//		return price;
-//	}
-//
-//
-//	public void setPrice(Double price) {
-//		this.price = price;
-//	}
-//
-//
-//	public Double getCalories() {
-//		return calories;
-//	}
-//
-//
-//	public void setCalories(Double calories) {
-//		this.calories = calories;
-//	}
+    public Drink(String name, Double price, Double calories) {
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+    }
 
 
-	@Override
-	public String getMenuItemLine() {
-		return name + " prezzo: " + price + " calorie: " + calories;
-	}
-	
-	
-	
-	
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Double getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public Double getCalories() {
+        return calories;
+    }
+
+
+    public void setCalories(Double calories) {
+        this.calories = calories;
+    }
+
+
+    @Override
+    public String getMenuItemLine() {
+        return name + " prezzo: " + price + " calorie: " + calories;
+    }
+
+
+
+
 }
